718bdb1220c5fc2185977ae6a2745668d6ee279c
daflyinbed <1677759063@qq.com>
2024-04-30 18:16:19 +0800

    feat: add script

---
+++ maintenance/purge.sh
@@ -0,0 +1,1 @@
+php maintenance/purgePage.php < etc/purge_pages.txt > /usr/local/var/log/php/purge.prts.log
---
+++ maintenance/sitemap.sh
@@ -0,0 +1,1 @@
+php maintenance/generateSitemap.php --fspath=sitemap/ > /usr/local/var/log/php/sitemap.prts.log
